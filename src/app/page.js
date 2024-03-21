8eaaa23c060b09d1c4e9224da291764e074c1a61
Wijayaac <wijayaac25@gmail.com>
2024-03-21 10:41:22 +0700

    Add : product listing on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,135 @@
+import { shopifyFetch } from "@/utils/shopify";
 import Image from "next/image";
 
-export default function Home() {
+// TODO refactor to query utils
+export async function getAllProducts() {
+  return shopifyFetch({
+    query: `{
+			products(first: 12) {
+				edges {
+					node {
+						id
+						title
+						handle
+						description
+						images(first: 1) {
+							edges {
+								node {
+									originalSrc
+									altText
+								}
+							}
+						}
+						priceRange {
+							minVariantPrice {
+								amount
+								currencyCode
+							}
+						}
+					}
+				}
+			}
+		}
+		`,
+  });
+}
+
+// TODO: refactor to query utils
+export async function getHomeCollection() {
+  return shopifyFetch({
+    query: `{
+			collectionByHandle(handle: "sale") {
+				products(first: 3) {
+					edges {
+						node {
+							id
+							title
+							handle
+							images(first: 1) {
+								edges {
+									node {
+										originalSrc
+										altText
+									}
+								}
+							}
+							priceRange {
+								minVariantPrice {
+									amount
+									currencyCode
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		`,
+  });
+}
+
+export default async function Home() {
+  const {
+    body: {
+      data: { products },
+    },
+  } = await getAllProducts();
+
+  const {
+    body: {
+      data: {
+        collectionByHandle: { products: homeCollection },
+      },
+    },
+  } = await getHomeCollection();
+  console.log(homeCollection);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className='flex min-h-screen flex-col items-center justify-between px-8'>
+      <div className='grid md:grid-cols-5 md:grid-rows-4 gap-4 md:my-10'>
+        {/* TODO: sale product collection refactor */}
+        {homeCollection &&
+          homeCollection.edges.map(({ node }, idx) => (
+            <div
+              key={node.id}
+              className={`relative group overflow-hidden rounded-xl ${idx === 0 ? "md:col-span-3 md:row-span-4" : "md:col-span-2 md:row-span-2 md:col-start-4"} ${
+                idx === homeCollection.length - 1 ? "md:row-start-3" : ""
+              } transition duration-300 ease-in-out border dark:border-black hover:border-blue-300`}>
+              <Image className='relative h-full w-full object-cover transition duration-300 ease-in-out group-hover:scale-105' src={node.images?.edges[0]?.node?.originalSrc} alt={node.images?.edges[0]?.node.altText} width={1080} height={1080} />
+              <div className={`absolute ml-2 p-2 bottom-0 left-0 dark:bg-black rounded-full ${idx === 0 ? "md:mb-[50%]" : "md:mb-10"} mb-4`}>
+                <div className='flex md:gap-2 items-center'>
+                  <p className='text-xs md:text-sm pl-2'>{node.title}</p>
+                  <p className='flex items-center justify-center gap-1 bg-blue-500 rounded-full px-2 py-1 min-w-[80px] md:text-sm'>
+                    {node.priceRange.minVariantPrice.amount} <span className='text-[10px]'>{node.priceRange.minVariantPrice.currencyCode}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* TODO: refactor to component */}
+      <div className='flex flex-wrap flex md:my-20 my-10'>
+        {products &&
+          products.edges.map(({ node }) => (
+            <div key={node.id} className='md:basis-1/3 my-2 px-2'>
+              <div className='relative group rounded-xl overflow-hidden border border-black hover:border-blue-400'>
+                <Image
+                  className='w-full h-full relative object-cover transition duration-300 ease-in-out group-hover:scale-105 group'
+                  src={node.images?.edges[0]?.node?.originalSrc}
+                  alt={node.images?.edges[0]?.node.altText}
+                  width={480}
+                  height={480}
+                />
+                <div className='absolute mb-4 mx-2 bottom-0 left-0 dark:bg-black rounded-full'>
+                  <div className='flex items-center gap-2 p-2'>
+                    <p className='text-xs md:text-sm pl-2'>{node.title}</p>
+                    <p className='flex items-center justify-center gap-1 md:text-sm h-full bg-blue-500 px-2 py-1 rounded-full min-w-[80px] grow'>
+                      {node.priceRange.minVariantPrice.amount} <span className='text text-[10px]'>{node.priceRange.minVariantPrice.currencyCode}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </main>
   );
