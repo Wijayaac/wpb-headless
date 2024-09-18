c9054bb4599f2d9dad8e024e855affaa24633d50
Wijayaac <wijayaac25@gmail.com>
2024-09-18 20:06:59 +0700

    feat: adding recaptcha from frontend

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,12 +3,47 @@ import { Resend } from "resend";
 import * as React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+interface CloudflareTurnstileResponse {
+  success: boolean;
+  "errors-codes": string[];
+  challenge_ts: string;
+  hostname: string;
+}
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { name, address, state, zip, message, products, estimatedTotal } =
-      body;
+    const {
+      name,
+      address,
+      state,
+      zip,
+      message,
+      products,
+      estimatedTotal,
+      token,
+    } = body;
+
+    const turnsstileRequest = await fetch(
+      `https://challenges.cloudflare.com/turnstile/v0/siteverify`,
+      {
+        method: "POST",
+        body: `secret=${encodeURIComponent(
+          process.env.TURNSTILE_SECRET_KEY!,
+        )}&response=${encodeURIComponent(token)}`,
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+        },
+      },
+    );
+
+    const turnstileResponse =
+      (await turnsstileRequest.json()) as CloudflareTurnstileResponse;
+
+    if (!turnstileResponse.success) {
+      return Response.json({ message: "Invalid token" }, { status: 400 });
+    }
+
     const { data, error } = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: ["wijayaac25@gmail.com"],
